739f2583a31f393f89b3d7ff919aecb41e6305ad
psshamshin <57655945+psshamshin@users.noreply.github.com>
2022-09-15 18:06:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var animation = bodymovin.loadAnimation({
+  container: document.querySelector(".fix-place"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "fix.json",
+});
+var animation = bodymovin.loadAnimation({
+  container: document.querySelector(".piat-place"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "5-ka-info.json",
+});
